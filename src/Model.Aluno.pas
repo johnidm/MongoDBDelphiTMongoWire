e1f8c8a4c92f0b0261bafd4b29b092c678561f53
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2013-09-17 21:55:26 -0300

    Liberacao do projeto

---
+++ src/Model.Aluno.pas
@@ -0,0 +1,23 @@
+unit Model.Aluno;
+
+interface
+
+uses
+  Generics.Collections;
+
+type
+  TModelAluno = class
+  private
+    FCodigo: Integer;
+    FNome: string;
+  public
+    property Codigo:  Integer read FCodigo write FCodigo;
+    property Nome: string read FNome write FNome;
+  end;
+
+  TModelListaAlunos= class(TObjectList< TModelAluno >)
+  end;
+
+implementation
+
+end.
